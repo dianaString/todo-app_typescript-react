09beda190c080f283347b1634a5b4e096a6df52c
dianaString <dianamarmolus@gmail.com>
2024-01-16 09:46:17 +0100

    Update from draft

---
+++ README.md
@@ -0,0 +1,34 @@
+# Todo App en React con TypeScript
+
+![](<todo-example.gif>)
+
+### Primera toma de contacto con TypeScript!
+Gracias al tutorial de midudev "[Tutorial práctico: React y TypeScript paso a paso](https://www.youtube.com/watch?v=4lAYfsq-2TE)" por enseñarme las bases de este lenguaje de programación 😄
+
+
+Fondo animado inspirado en "[Pure CSS Animated Gradient Background](https://codepen.io/redstapler/pen/LMyLxP)" de Red Stapler
+
+
+
+<details>
+  <summary> (mostrar lista de objetivos conseguidos)</summary>
+</br>
+	
+- [x] Inicializar proyecto con Vite
+- [x] Añadir linter para TypeScript + React (y Prettier)
+- [x] Añadir estilos del TodoMVC
+- [x] Listar todos los TODOs
+- [x] Poder borrar un TODO
+- [x] Marcar TODO como completado
+- [x] Añadir forma de filtrar TODOs (Footer)
+- [x] Mostrar número de TODOs pendientes (Footer)
+- [x] Añadir forma de borrar todos los TODOs completados
+- [x] Crear Header con input (Header)
+- [x] Crear un TODO (Header)
+- [x] Customizar los estilos de TodoMVC
+- [X] Añadir animaciones con framer-motion
+- [ ] Poder editar el texto de un TODO (Doble click)
+- [ ] Pasar a Reducer
+- [ ] Sincronizar con backend
+  
+</details>
---
+++ src/App.tsx
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+import { TODO_FILTERS } from './consts'
+import {
+	type TodoTitle,
+	type FilterValue,
+	type TodoId,
+	type Todo as TodoType,
+} from './types'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+
+const mockTodos = [
+	{
+		id: '1',
+		title: 'Ver el amanecer al menos una vez al día',
+		completed: true,
+	},
+	{
+		id: '2',
+		title: 'Averiguar quién metió la calculadora en gelatina',
+		completed: false,
+	},
+	{
+		id: '3',
+		title: 'Comprobar si es cierto que nunca ocurre nada bueno después de las 2 AM',
+		completed: false,
+	},
+]
+
+const App = (): JSX.Element => {
+	const [todos, setTodos] = useState(mockTodos)
+	const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
+
+	const handleRemove = ({ id }: TodoId): void => {
+		const newTodos = todos.filter(todo => todo.id !== id)
+		setTodos(newTodos)
+	}
+
+	const handleCompleted = ({
+		id,
+		completed,
+	}: Pick<TodoType, 'id' | 'completed'>): void => {
+		const newTodos = todos.map(todo => {
+			if (todo.id === id) {
+				return {
+					...todo,
+					completed,
+				}
+			}
+			return todo
+		})
+
+		setTodos(newTodos)
+	}
+
+	const handleFilterChange = (filter: FilterValue): void => {
+		setFilterSelected(filter)
+	}
+
+	const handleRemoveAllCompleted = (): void => {
+		const newTodos = todos.filter(todo => !todo.completed)
+		setTodos(newTodos)
+	}
+
+	const activeCount = todos.filter(todo => !todo.completed).length
+	const completedCount = todos.length - activeCount
+
+	const filteredTodos = todos.filter(todo => {
+		if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+		if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+		return todo
+	})
+
+	const handleAddTodo = ({ title }: TodoTitle): void => {
+		const newTodo = {
+			title,
+			id: crypto.randomUUID(),
+			completed: false,
+		}
+
+		const newTodos = [...todos, newTodo]
+		setTodos(newTodos)
+	}
+
+	return (
+		<>
+			<div className='todoapp'>
+				<Header onAddTodo={handleAddTodo} />
+				<Todos
+					onToggleCompleteTodo={handleCompleted}
+					onRemoveTodo={handleRemove}
+					todos={filteredTodos}
+				/>
+				<Footer
+					activeCount={activeCount}
+					completedCount={completedCount}
+					filterSelected={filterSelected}
+					onClearCompleted={handleRemoveAllCompleted}
+					handleFilterChange={handleFilterChange}
+				/>
+			</div>
+		</>
+	)
+}
+
+export default App
